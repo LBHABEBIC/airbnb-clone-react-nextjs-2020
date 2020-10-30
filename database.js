91e06105aab7cf83b22a76414d9fe97345c48c3b
Flavio Copes <copesc@gmail.com>
2020-10-30 19:48:21 +0100

    Implement model and DB connection

---
+++ database.js
@@ -0,0 +1,4 @@
+export const user = 'flavio'
+export const password = ''
+export const host = 'localhost'
+export const database = 'nextbnb'
--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,8 +1,28 @@
-export default (req, res) => {
+import { User, sequelize } from '../../../model.js'
+
+const randomString = (length) => {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let result = ''
+  for (let i = length; i > 0; --i) {
+    result += chars[Math.floor(Math.random() * chars.length)]
+  }
+  return result
+}
+
+export default async (req, res) => {
   if (req.method !== 'POST') {
     res.status(405).end() //Method Not Allowed
     return
   }
-  console.log(req.body)
-  res.end()
+
+  const { email, password, passwordconfirmation } = req.body
+
+  if (password !== passwordconfirmation) {
+    res.end(
+      JSON.stringify({ status: 'error', message: 'Passwords do not match' })
+    )
+    return
+  }
+  const user = await User.create({ email, password })
+  res.end(JSON.stringify({ status: 'success', message: 'User added' }))
 }
